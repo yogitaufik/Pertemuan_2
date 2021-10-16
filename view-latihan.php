a084071e1246aca90199da34e1967f37c3dc5334
yogitaufik <90616708+yogitaufik@users.noreply.github.com>
2021-10-16 16:10:20 +0700

    Create view-latihan.php

---
+++ view-latihan.php
@@ -0,0 +1,13 @@
+<html>
+    <head> <title>Latihan1</title>
+    </head>
+    <body>
+        Halo Kawan.. Yuk Kita Belajar Web Programming..!!!
+        <br>
+        Nilai 1 = <?= $nilai1; ?> Nilai 2 = <?= $nilai2; ?>
+        <br>
+        ini hasil dari pemodelan dengan methode penjumlahan yaitu
+
+        <?php echo "$nilai1 + $nilai2 = $hasil"; ?>
+    </body>
+</html>
